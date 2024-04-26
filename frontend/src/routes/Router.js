256431ae933c82d33c6a2afec6ea1bceccd51f88
Sulakshani99 <139636218+Sulakshani99@users.noreply.github.com>
2024-04-26 09:20:42 +0530

    Initial commit

---
+++ frontend/src/routes/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+import Bookings from "../pages/Bookings";
+import SellCar from "../pages/SellCar";
+import Settings from "../pages/Settings";
+// import AddVehicleForm from "../components/AddVehicleForm";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Navigate to="/dashboard" element={<Dashboard />} />}
+      />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/bookings" element={<Bookings />} />
+      <Route path="/sell-car" element={<SellCar />} />
+      <Route path="/settings" element={<Settings />} />
+      {/* <Route path="/add-vehicle" element={<AddVehicleForm/>} /> */}
+    </Routes>
+  );
+};
+
+export default Router;
